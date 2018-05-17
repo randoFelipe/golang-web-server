583e55df2088f6429b5fd0ad83fa1c817c921b6e
Felipe Rando <felipe.rando@rivendel.com.br>
2018-05-17 10:45:26 -0300

    trying rolling deploy with deploy.sh

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "The cake is a lie!")
+	io.WriteString(w, "Não para não para não")
 }
 
 func sayMyName(w http.ResponseWriter, r *http.Request){
