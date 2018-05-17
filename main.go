3efca7613be43d096e6afd93cd4cc8c5fe01fe03
Felipe Rando <felipe.rando@rivendel.com.br>
2018-05-17 09:41:52 -0300

    added sayMyName function

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "The cake is a lie!")
 }
 
+func sayMyName(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w, "Hello Heisenberg")
+}
+
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/danger", sayMyName)
 	http.ListenAndServe(":8000", nil)
 }
