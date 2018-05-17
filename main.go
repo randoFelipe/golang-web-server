576fe0c8a47a8f599216b5af56c745017af152fd
Felipe Rando <felipe.rando@rivendel.com.br>
2018-05-17 09:55:46 -0300

    added scrum func

--- main.go
+++ main.go
@@ -13,8 +13,13 @@ func sayMyName(w http.ResponseWriter, r *http.Request){
 	io.WriteString(w, "Hello Heisenberg")
 }
 
+func melhorScrum(w http.ResponseWriter, r *http.Request){
+	io.WriteString(w, "Hello rodolpha1")
+}
+
 func main() {
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/danger", sayMyName)
+	http.HandleFunc("/scrum", melhorScrum)
 	http.ListenAndServe(":8000", nil)
 }
