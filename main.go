78d7c9d5fbe72b125cd1332dda7b04a0f8c3fe62
Felipe Rando <felipe.rando@rivendel.com.br>
2018-05-17 12:03:40 -0300

    testing new task definition

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Não para não para não")
+	io.WriteString(w, "mudou task definition")
 }
 
 func sayMyName(w http.ResponseWriter, r *http.Request){
