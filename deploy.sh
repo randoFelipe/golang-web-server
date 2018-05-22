2b39eabe971b1f9579b67ffab44f33ccc258d03c
randoFelipe <felipe.rando@rivendel.com.br>
2018-05-22 14:54:37 -0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -48,7 +48,9 @@ make_task_def(){
 			"essential": true,
 			"memory": 200,
 			"cpu": 10,
-			"requiresCompatibilities" : "FARGATE",
+			"requiresCompatibilities": [
+        			"FARGATE"
+    			],
 			"portMappings": [
 				{
 					"containerPort": 8000
