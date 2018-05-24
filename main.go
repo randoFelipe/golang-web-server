a00607e99f075314a811789434d52a34f4fafc2a
randoFelipe <felipe.rando@rivendel.com.br>
2018-05-24 15:58:16 -0300

    Update main.go

--- main.go
+++ main.go
@@ -3,10 +3,25 @@ package main
 import (
 	"io"
 	"net/http"
+	"os"
+	"fmt"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "mudou task definition")
+}
+
+func test_service_discovery(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.Get("http://golang-webserver.local")
+	if err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	
+	bs := make ([]byte, 9999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
+	
 }
 
 func sayMyName(w http.ResponseWriter, r *http.Request){
@@ -21,5 +36,6 @@ func main() {
 	http.HandleFunc("/", hello)
 	http.HandleFunc("/danger", sayMyName)
 	http.HandleFunc("/scrum", melhorScrum)
+	http.HandleFunc("/dnsstatus", test_service_discovery)
 	http.ListenAndServe(":8000", nil)
 }
